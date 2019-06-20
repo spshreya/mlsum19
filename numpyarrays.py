69c4378913222dc5a540385abe0b271d3181a2c0
spshreya <shreya.pandey.emip@gmail.com>
2019-06-20 05:59:51 +0000

    basics of arrays using numpy (python)

---
+++ numpyarrays.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+a=np.array([2,5,7,9])                                                                                                                                
+print('a=')                                                                                                                                          
+print(a)                                                                                                                                             
+type(a)                                                                                                                                              
+                                                                                                                                                     
+#multidimensional array                                                                                                                              
+b= np.array([[2,5,7],[3,4,5]])                                                                                                                       
+print('b=')                                                                                                                                          
+print(b)                                                                                                                                             
+                                                                                                                                                     
+#knowing dimensions of the array                                                                                                                     
+print(b.shape)                                                                                                                                       
+                                                                                                                                                     
+#retrieving data                                                                                                                                     
+#first row                                                                                                                                           
+print(b[0])                                                                                                                                          
+                                                                                                                                                     
+#first and second row                                                                                                                                
+print(b[[0,1]])                                                                                                                                      
+                                                                                                                                                     
+#first to last row                                                                                                                                   
+print(b[0:])                                                                                                                                         
+                                                                                                                                                     
+#first to second row                                                                                                                                 
+print(b[0:2])                                                                                                                                        
+#last one gets excluded                                                                                                                              
+                                                                                                                                                     
+#all rows and first col                                                                                                                              
+print(b[0:,0])              
